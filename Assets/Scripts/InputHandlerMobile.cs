f02cba6085351d19d4a97570352ae8f644379a85
German Quero <querojimenezg@gmail.com>
2020-03-05 20:04:08 +0100

    Organizado toda la vaina de los inputhandlers

--- Assets/Scripts/InputHandlerMobile.cs
+++ Assets/Scripts/InputHandlerMobile.cs
@@ -2,41 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour
+public class InputHandlerMobile : MonoBehaviour
 {
     Locomotor locomotor = null;
     Bomber bomber = null;
-    private GameObject turret;
     public GameObject player;
+    private GameObject turret;
     public Joystick moveJoystick ,aimJoystick;
     private Vector3 aim;
-
     // Start is called before the first frame update
     void Start()
     {
         locomotor = player.GetComponent<Locomotor>();
         bomber = player.GetComponent<Bomber>();
         turret = player.transform.GetChild(0).gameObject;
-        moveJoystick.gameObject.SetActive(false);
-        aimJoystick.gameObject.SetActive(false);
+        moveJoystick.gameObject.SetActive(true);
+            aimJoystick.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         float h, v;
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        h = moveJoystick.Horizontal;
+        v = moveJoystick.Vertical;
         locomotor.MoveTo(h, v);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            bomber.SetBomb();
-        }
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        turret.GetComponent<Turret>().rotateTurret(ray);
-        if (Input.GetMouseButtonDown(0))
-        {
-            turret.GetComponent<Turret>().doShot();
-        }
+        //hacer boton pa las bombas
+        aim = new Vector3(aimJoystick.Horizontal, 0, aimJoystick.Vertical) * 10;
+        turret.GetComponent<Turret>().aimTurret(aim);
+        //hacer boton pa disparar
+
     }
+    public void shootMobile(){
+      turret.GetComponent<Turret>().doShot();
+   }
 }
