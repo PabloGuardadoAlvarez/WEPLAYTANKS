e102d7bd1c05ceee7ab727386240df7508819916
Pablo Guardado <pabloguardadoalvarez@gmail.com>
2020-04-24 14:03:00 +0200

    Menu principal y de opciones anadido

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject mainMenu, optionsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void changeToOptions() {
+
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+
+}
